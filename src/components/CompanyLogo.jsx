60263ba5aa3d3a8b7a1c99eff52895484e61dc96
oladamilare10 <oladamilare10@gmail.com>
2024-06-25 16:44:25 -0700

    Gideon AI App with react JS and tailwind CSS

---
+++ src/components/CompanyLogo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { companyLogos } from "../constants";
+
+const CompanyLogo = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        Helping people create beautiful contents
+      </h5>
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li
+            key={index}
+            className="flex items-center justify-center flex-1 h-[8rem]"
+          >
+            <img src={logo} alt={logo} width={134} height={28} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CompanyLogo;
